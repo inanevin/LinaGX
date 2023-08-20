6a0aea7fda211d2fd3513fb41aa6a35c2e1609f5
Inan Evin <inanevin@gmail.com>
2023-08-20 17:26:30 +0300

    project structure

--- CMake/ExampleProjectSettings.cmake
+++ CMake/ExampleProjectSettings.cmake
@@ -12,6 +12,6 @@ if(WIN32)
 add_custom_command(
 TARGET ${PROJECT_NAME}
 POST_BUILD
-COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}/../../_Vendor/bin/" "${CMAKE_BINARY_DIR}/bin/${PROJECT_NAME}/$<CONFIGURATION>/")
+COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}/../../Dependencies/bin/" "${CMAKE_BINARY_DIR}/bin/${PROJECT_NAME}/$<CONFIGURATION>/")
 
 endif()
