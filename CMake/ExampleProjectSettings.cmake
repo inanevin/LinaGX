0d6f36ccb3c3f9bfecca9048c40945592b98556c
Inan Evin <inanevin@gmail.com>
2023-12-04 16:41:22 +0200

    sync

--- CMake/ExampleProjectSettings.cmake
+++ CMake/ExampleProjectSettings.cmake
@@ -32,11 +32,11 @@
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 set_property(TARGET ${PROJECT_NAME} PROPERTY FOLDER LinaGXProject/Examples)
+set(LINAGX_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/${PROJECT_NAME}/$<CONFIGURATION>/)
+message(${LINAGX_RUNTIME_OUTPUT_DIRECTORY})
 
 if(WIN32)
-
 set_property(TARGET ${PROJECT_NAME} PROPERTY VS_DEBUGGER_WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/bin/${PROJECT_NAME}/$<CONFIGURATION>/")
-
 endif()
 
 if(APPLE)
@@ -56,14 +56,6 @@ TARGET ${PROJECT_NAME}
 POST_BUILD
 COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}/Resources/" "${CMAKE_BINARY_DIR}/bin/${PROJECT_NAME}/$<CONFIGURATION>/Resources/")
 
-## Copy windows dlls
-if(WIN32)
-add_custom_command(
-TARGET ${PROJECT_NAME}
-POST_BUILD
-COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}/../../Dependencies/bin/" "${CMAKE_BINARY_DIR}/bin/${PROJECT_NAME}/$<CONFIGURATION>/")
-
-endif()
 
 if(APPLE)
 
